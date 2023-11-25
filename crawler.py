e0e42f8bcb7262b199d3d6ad6693a2da25afc541
nathanvpham <natepham16@gmail.com>
2023-11-24 22:30:27 -0800

    added the ability to presist data to mongoDB

--- crawler.py
+++ crawler.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
+from pymongo import MongoClient  
 import re
 
 
@@ -20,30 +21,60 @@ def main():
 
     frontier = ['https://www.cpp.edu/sci/computer-science/']
     visitedURL = []
+    db = connectDataBase()
+    collection = db.documents
 
     while frontier:
         url = frontier.pop(0)
         visitedURL.append(url)
-        html = urlopen(url)
-        storePage(url, html)
-        bs = BeautifulSoup(html.read(), 'html.parser')
-        if bs.find_all('h1', text = re.compile('.*Permanent Faculty.*')):
-            break
         try:
-            currentURL = bs.find_all('a', href=True)
+            html = urlopen(url)
         except:
             continue
-        for link in currentURL:
-            link = link.get('href')
-            if link[0] == "/" or link[0:4] == "http":
-                if link [0] == "/":
-                    link = "https://www.cpp.edu" + link
-                if link not in visitedURL and link not in frontier:
-                    frontier.append(link)
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        storePage(collection, url, bs.prettify())
+        if bs.find_all('h1', text = re.compile('.*Permanent Faculty.*')):
+            break
+        currentURL = bs.find_all('a', href=True)
+        if currentURL:
+            for link in currentURL:
+                link = link.get('href')
+                if link[0] == "/" or link[0:4] == "http":
+                    if link [0] == "/":
+                        link = "https://www.cpp.edu" + link
+                    if link not in visitedURL and link not in frontier:
+                        frontier.append(link)
 
         
-def storePage(url, html):
+def storePage(collection, url, html):
+
+    document = {
+        "url": url,
+        "html": html
+    }
+    
+    collection.insert_one(document)
     return
+
+def connectDataBase():
+
+    # Create a database connection object using pymongo
+    # --> add your Python code here
+    DB_NAME = "pages"
+    DB_HOST = "localhost"
+    DB_PORT = 27017
+
+    try:
+
+        client = MongoClient(host=DB_HOST, port=DB_PORT)
+        db = client[DB_NAME]
+        print("Connected to MongoDB")
+
+        return db
+
+    except:
+        print("Database not connected successfully")
+
         
 
 
